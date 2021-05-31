9ffff460a749fcbab34afa889f2b194e8ca091ac
Varun Bandarupalli <varuvirus@hotmail.com>
2021-05-30 21:05:34 -0700

    Initial Commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programming_practice_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
